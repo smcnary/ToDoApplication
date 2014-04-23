6cfd5b6ca0c4028fef0e11c9cd883beef1d86442
Giltain <jeremy.doucet93@gmail.com>
2014-04-23 13:08:22 -0500

    Added code for sam

---
+++ ToDoApplication/ForSam.js
@@ -0,0 +1,19 @@
+﻿//document.getElementById("todoInput").value;
+//document.getElementById("todoList").innerHTML;
+
+"use strict";
+var toDoCounter = 0; //must put in global scope so it doesnt get reset
+
+function submitItem() {
+    "use strict";
+    toDoCounter++;
+    var inputValue = document.getElementById("todoInput");
+    var outputValue = document.getElementById("todoList");
+
+    outputValue.innerHTML += '<li id="item' + toDoCounter + '" onclick="strikeThrough(\'item' + toDoCounter + '\');">'
+        + inputValue.value + '</li>';
+}
+function strikeThrough(listItemId) {
+    "use strict";
+    document.getElementById(listItemId).setAttribute("class", "strikethrough");
+}
---
+++ ToDoApplication/classJavaScript.js
@@ -0,0 +1,26 @@
+﻿//document.getElementById("todoInput").value;
+//document.getElementById("todoList").innerHTML;
+
+"use strict";
+var toDoCounter = 0; //must put in global scope so it doesnt get reset
+
+function submitItem() {
+    "use strict";
+    toDoCounter++;
+    var inputValue = document.getElementById("todoInput");
+    var outputValue = document.getElementById("todoList");
+
+    outputValue.innerHTML += '<button id="button' + toDoCounter
+        + '" type="button" onclick="hideList(\'item' + toDoCounter + '\', \'button' + toDoCounter + '\');">X</button>'
+        + '<li id="item' + toDoCounter + '" onclick="strikeThrough(\'item' + toDoCounter + '\');">'
+        + inputValue.value + '</li>';
+}
+function strikeThrough(listItemId) {
+    "use strict";
+    document.getElementById(listItemId).setAttribute("class", "strikethrough");
+}
+function hideList(listItemId, buttonItemId) {
+    "use strict";
+    document.getElementById(listItemId).setAttribute("class", "hideList");
+    document.getElementById(buttonItemId).setAttribute("class", "hideList");
+}
